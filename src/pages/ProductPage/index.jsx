da47b12ab07c7ed47851b4dec0c30fc67dfa67f7
DEFKIRCH <thijmen.kirch@gmail.com>
2022-06-08 12:34:26 +0200

    Started on category sidebar and have fetching of items working

--- src/pages/ProductPage/index.jsx
+++ src/pages/ProductPage/index.jsx
@@ -1,6 +1,52 @@
+import "./style.css"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const ProductPage = () => {
+const [products, setProducts] = useState([]);
+const [categories, setCategories ] = useState([]);
+const URL = "http://localhost:4000";
+// Fetching all products
+useEffect(() => {
+  const fetchProducts = async (req, res) => {
+    try {
+      const allProducts = await axios.get(`${URL}/products`);
+      // console.log("Products: ", res.data);
+      setProducts(allProducts.data)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+  fetchProducts();
+}, [])
+
+// Fetching all categories
+useEffect(() => {
+  const fetchCategories = async (req, res) => {
+    try {
+      const allCategories = await axios.get(`${URL}/categories`)
+      setCategories(allCategories.data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+  fetchCategories();
+}, [])
+
+
   return (
-    <div>ProductPage</div>
+    <div className="Container">
+      <div>
+        <h4>Categories</h4>
+        <ul>
+          {categories.map(category => {
+            return (
+              <li key={category.id}>{category.title}</li>
+            )
+          })}
+        </ul>
+      </div>  
+      
+    </div>
   )
 }
