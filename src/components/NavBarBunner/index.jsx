441fdac24cd12e99de223bb4ea241f3f46ce04f2
DEFKIRCH <thijmen.kirch@gmail.com>
2022-06-09 14:15:46 +0200

    Changes to banner image

--- src/components/NavBarBunner/index.jsx
+++ src/components/NavBarBunner/index.jsx
@@ -7,7 +7,7 @@ const NavBarBunner = () => {
           <h3>Product for every need,at the distance of a click</h3>
         </div>
         <div className="NavBar-picture">
-          <img className="nav_picture" alt="navbar bunner" />
+          {/* <img className="nav_picture" src="https://images.unsplash.com/photo-1582533575066-75bd83ac91de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="navbar bunner" /> */}
         </div>
       </div>
     </div>
