4619dbf9edfd2c133042e5e9f44db66b0dc57493
DEFKIRCH <thijmen.kirch@gmail.com>
2022-06-09 12:18:30 +0200

    removed review link from components export in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export * from "./NavBar";
 export * from "./SideBar";
 export * from "./ProductCard";
-export * from "./Review";
 export * from "./Pagination";
 export * from "./ReviewTab";
 export * from "./ProductInfo";
