d7bc79508305f724c7e0c8a495b840253ca629ad
MartaPanasyuk <mart.panasyuk@gmail.com>
2022-06-09 15:14:08 +0200

    adding styles for Login form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   HomePage,
   ProductPage,
   ProductDetailsPage,
+  LoginPage,
 } from "./pages";
 
 function App() {
@@ -22,7 +23,9 @@ function App() {
         <Route path="/products/:productId" element={<ProductDetailsPage />} />
         <Route path="/categories/:categoryId" element={<CategoryPage />} />
         <Route path="/contacts" element={<AboutUsPage />} />
+        <Route path="/user/login" element={<LoginPage />} />
       </Routes>
+
       <Footer />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export * from "./AboutUsPage";
 export * from "./HomePage";
 export * from "./ProductPage";
 export * from "./ProductDetailsPage";
+export * from "./LoginPage";
